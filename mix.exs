1a15a2757642bd039d9245e6dfec6227a3fe1909
Soo Yang <sooyang.code@gmail.com>
2024-06-26 22:16:24 +0800

    Install nimble_csv

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule BirdieMachine.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:nimble_csv, "~> 1.1"}
     ]
   end
 
